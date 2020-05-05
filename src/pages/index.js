675033b1b41219eb6467803e301e174bcf1cf238
João Oliveira <joao.oliveira@spltech.co.uk>
2020-05-05 12:24:57 +0100

    Video Player

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Player } from 'video-react';
+import "../styles/video-react/video-react.scss"; // or import scss
 
 import Layout from "../components/layout"
 
@@ -7,7 +9,11 @@ import "../styles/pages.scss"
 
 const IndexPage = () => (
   <Layout title="Democracy. Diversity. Opportunity">
-    <Link to="/landing"><div id="index-background"></div></Link>
+    <Player
+      playsInline
+      poster="/images/index_placeholder.png"
+      src="https://embed-ssl.wistia.com/deliveries/456a9f531d806cb8987f184f45b9781f.mp4"
+    />
   </Layout>
 )
 
